4dab82388fde4a4a8c1ae11236dd464c73e81bc0
LindsayBradford <lindsay.w.bradford@gmail.com>
2019-02-06 16:59:47 +1000

    Small fix to broken test.

--- internal/pkg/dataset/tables/NullTable.go
+++ internal/pkg/dataset/tables/NullTable.go
@@ -9,9 +9,10 @@ type NullTable struct{}
 func (nt *NullTable) Name() string        { return "Null" }
 func (nt *NullTable) SetName(name string) {}
 
-func (nt *NullTable) Cell(xPos uint, yPos uint) interface{}           { return "null" }
-func (nt *NullTable) CellFloat64(xPos uint, yPos uint) float64        { return float64(0) }
-func (nt *NullTable) CellInt64(xPos uint, yPos uint) int64            { return int64(0) }
+func (nt *NullTable) Cell(xPos uint, yPos uint) interface{}    { return "null" }
+func (nt *NullTable) CellFloat64(xPos uint, yPos uint) float64 { return float64(0) }
+func (nt *NullTable) CellString(xPos uint, yPos uint) string   { return "0" }
+
 func (nt *NullTable) SetCell(xPos uint, yPos uint, value interface{}) {}
 
 func (nt *NullTable) SetSize(colNum uint, rowNum uint) {}
