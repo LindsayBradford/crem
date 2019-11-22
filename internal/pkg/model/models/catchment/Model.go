d00d3c23dcab87ffc13258a64b0421d8a61f1bfe
Lindsay Bradford <lindsay.w.bradford@gmail.com>
2019-11-22 11:01:57 +1000

    Retiring SedimentVsCost decision variable.

--- internal/pkg/model/models/catchment/Model.go
+++ internal/pkg/model/models/catchment/Model.go
@@ -5,17 +5,13 @@ package catchment
 import (
 	"os"
 	"path/filepath"
-	"strconv"
-
-	"github.com/LindsayBradford/crem/internal/pkg/model"
-	"github.com/LindsayBradford/crem/internal/pkg/model/models/catchment/variables"
-	"github.com/LindsayBradford/crem/internal/pkg/rand"
-	"github.com/LindsayBradford/crem/pkg/logging/loggers"
-	"github.com/LindsayBradford/crem/pkg/strings"
 
 	"github.com/LindsayBradford/crem/internal/pkg/dataset/excel"
+	"github.com/LindsayBradford/crem/internal/pkg/model"
 	"github.com/LindsayBradford/crem/internal/pkg/model/models/catchment/parameters"
 	baseParameters "github.com/LindsayBradford/crem/internal/pkg/parameters"
+	"github.com/LindsayBradford/crem/internal/pkg/rand"
+	"github.com/LindsayBradford/crem/pkg/logging/loggers"
 	"github.com/LindsayBradford/crem/pkg/threading"
 )
 
@@ -54,43 +50,6 @@ func (m *Model) Initialise() {
 	m.loadSourceDataSet()
 	m.CoreModel.Initialise()
 	m.RandomlyInitialiseActions()
-
-	// the SedimentVsCost variable needs values in both SedimentProduced and ImplementationCost in order to
-	// build an effective weighted unit-scaled vector.  If one of them is 0 (where implementation cost starts),
-	// we get numbers that report as infinity, breaking stuff.  I can't build that vector UNTIL I can guarantee
-	// that I can build a unit-scaled vector without issue.  For now, this is a spot where I can keep it, though it's
-	// conceptually a very poor location.
-
-	m.buildSedimentVsCostDecisionVariable()
-}
-
-func (m *Model) buildSedimentVsCostDecisionVariable() {
-	sedimentProduction := m.ContainedDecisionVariables.Variable(variables.SedimentProductionVariableName)
-	implementationCost := m.ContainedDecisionVariables.Variable(variables.ImplementationCostVariableName)
-
-	sedimentWeight := m.parameters.GetFloat64(parameters.SedimentProductionDecisionWeight)
-	implementationCostWeight := m.parameters.GetFloat64(parameters.ImplementationCostDecisionWeight)
-
-	sedimentVsCost, buildError := new(variables.SedimentVsCost).
-		Initialise().
-		WithObservers(m).
-		WithWeightedVariable(sedimentProduction, sedimentWeight).
-		WithWeightedVariable(implementationCost, implementationCostWeight).
-		Build()
-
-	if buildError != nil {
-		panic(buildError)
-	}
-
-	noteBuilder := new(strings.FluentBuilder).
-		Add(sedimentProduction.Name(), " weight = ", strconv.FormatFloat(sedimentWeight, 'f', 3, 64), ", ").
-		Add(implementationCost.Name(), " weight = ", strconv.FormatFloat(implementationCostWeight, 'f', 3, 64))
-
-	m.ObserveDecisionVariableWithNote(sedimentProduction, " Initial Value")
-	m.ObserveDecisionVariableWithNote(implementationCost, " Initial Value")
-	m.ObserveDecisionVariableWithNote(sedimentVsCost, noteBuilder.String())
-
-	m.ContainedDecisionVariables.Add(sedimentVsCost)
 }
 
 func (m *Model) loadSourceDataSet() {
