06ed1b29505aac9c59891dbbb72ad016cf72776a
Lindsay Bradford <lindsay.w.bradford@gmail.com>
2020-08-06 12:34:07 +1000

    Some refactoring around subcatcment handler.

--- cmd/cremengine/engine/api/v1subcatchmentHandler.go
+++ cmd/cremengine/engine/api/v1subcatchmentHandler.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/LindsayBradford/crem/internal/pkg/annealing/solution"
 	"github.com/LindsayBradford/crem/internal/pkg/model/planningunit"
 	"github.com/LindsayBradford/crem/internal/pkg/server/rest"
@@ -29,35 +30,48 @@ func (m *Mux) v1subcatchmentHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func (m *Mux) v1GetSubcatchmentHandler(w http.ResponseWriter, r *http.Request) {
-	pathElements := strings.Split(r.URL.Path, rest.UrlPathSeparator)
-	lastElementIndex := len(pathElements) - 1
-	subCatchmentAsString := pathElements[lastElementIndex]
+	requestSuppliedSubCatchment := deriveSubCatchmentFrom(r)
 
 	if m.modelSolution == nil {
-		m.Logger().Warn("Attempted to request subcatchment [" + subCatchmentAsString + "] state with no model present")
+		m.Logger().Warn("Attempted to request subcatchment [" + requestSuppliedSubCatchment + "] state with no model present")
 		m.NotFoundError(w, r)
 		return
 	}
 
-	subCatchmentAsInteger, convertError := strconv.Atoi(subCatchmentAsString)
-	if convertError != nil {
-		panic("Should not reach here -- regular expression map should stop non-integers from being passed to handler")
-	}
+	subCatchment := toPlanningUnitId(requestSuppliedSubCatchment)
 
-	subCatchment := planningunit.Id(subCatchmentAsInteger)
-
-	var subCatchmentFound bool
-	for _, action := range m.model.ManagementActions() {
-		if action.PlanningUnit() == subCatchment {
-			subCatchmentFound = true
-		}
-	}
-	if !subCatchmentFound {
-		m.Logger().Warn("Attempted to request subcatchment [" + subCatchmentAsString + "] state not offered by the model")
+	if !m.modelContains(subCatchment) {
+		m.Logger().Warn("Attempted to request subcatchment [" + requestSuppliedSubCatchment + "] state not offered by the model")
 		m.NotFoundError(w, r)
 		return
 	}
 
+	m.respondWithSubcatchmentState(w, subCatchment)
+}
+
+func (m *Mux) respondWithSubcatchmentState(w http.ResponseWriter, subCatchment planningunit.Id) {
+	scenarioName := m.Attribute(scenarioNameKey).(string)
+	responseMessage := fmt.Sprintf("Responding with model [%s] subcatchment [%d] state", scenarioName, subCatchment)
+	m.Logger().Info(responseMessage)
+
+	responseAttributes := m.deriveResponseAttributesFor(subCatchment)
+
+	restResponse := new(rest.Response).
+		Initialise().
+		WithWriter(w).
+		WithResponseCode(http.StatusOK).
+		WithCacheControlMaxAge(m.CacheMaxAge()).
+		WithJsonContent(responseAttributes)
+
+	writeError := restResponse.Write()
+
+	if writeError != nil {
+		wrappingError := errors.Wrap(writeError, "v1 subcatchment handler")
+		m.Logger().Error(wrappingError)
+	}
+}
+
+func (m *Mux) deriveResponseAttributesFor(subCatchment planningunit.Id) attributes.Attributes {
 	activeActions := m.modelSolution.ActiveManagementActions[subCatchment]
 	inactiveActions := m.modelSolution.InactiveManagementActions[subCatchment]
 
@@ -69,23 +83,7 @@ func (m *Mux) v1GetSubcatchmentHandler(w http.ResponseWriter, r *http.Request) {
 	for _, action := range inactiveActions {
 		returnAttributes = returnAttributes.Add(string(action), InactiveAction)
 	}
-
-	restResponse := new(rest.Response).
-		Initialise().
-		WithWriter(w).
-		WithResponseCode(http.StatusOK).
-		WithCacheControlMaxAge(m.CacheMaxAge()).
-		WithJsonContent(returnAttributes)
-
-	writeError := restResponse.Write()
-
-	scenarioName := m.Attribute(scenarioNameKey).(string)
-	m.Logger().Info("Responding with model [" + scenarioName + "] subcatchment [" + subCatchmentAsString + "] state")
-
-	if writeError != nil {
-		wrappingError := errors.Wrap(writeError, "v1 subcatchment handler")
-		m.Logger().Error(wrappingError)
-	}
+	return returnAttributes
 }
 
 func (m *Mux) v1PostSubcatchmentHandler(w http.ResponseWriter, r *http.Request) {
@@ -94,84 +92,20 @@ func (m *Mux) v1PostSubcatchmentHandler(w http.ResponseWriter, r *http.Request) 
 		return
 	}
 
-	pathElements := strings.Split(r.URL.Path, rest.UrlPathSeparator)
-	lastElementIndex := len(pathElements) - 1
-	subCatchmentAsString := pathElements[lastElementIndex]
+	requestSuppliedSubCatchment := deriveSubCatchmentFrom(r)
+	subCatchment := toPlanningUnitId(requestSuppliedSubCatchment)
 
-	subCatchmentAsInteger, convertError := strconv.Atoi(subCatchmentAsString)
-	if convertError != nil {
-		panic("Should not reach here -- regular expression map should stop non-integers from being passed to handler")
-	}
-
-	subCatchment := planningunit.Id(subCatchmentAsInteger)
-	var planningUnitFound bool
-	for _, value := range m.modelSolution.PlanningUnits {
-		if value == subCatchment {
-			planningUnitFound = true
-		}
-	}
-
-	if !planningUnitFound {
+	if !m.modelContains(subCatchment) {
 		m.NotFoundError(w, r)
 		return
 	}
 
-	scenarioName := m.Attribute(scenarioNameKey).(string)
-	m.Logger().Info("Processing POST of model [" + scenarioName + "] subcatchment [" + subCatchmentAsString + "] state")
-
-	requestContent := requestBodyToBytes(r)
-	postedAttributes := attributes.Attributes{}
-
-	unmnarshalError := json.Unmarshal(requestContent, &postedAttributes)
-	if unmnarshalError != nil {
-		wrappingError := errors.Wrap(unmnarshalError, "v1 POST subcatchment handler")
-		m.Logger().Error(wrappingError)
-		m.RespondWithError(http.StatusBadRequest, wrappingError.Error(), w, r)
+	processingError := m.processSubcatchmentPost(w, r, subCatchment)
+	if processingError != nil {
+		m.Logger().Error(processingError)
+		m.RespondWithError(http.StatusBadRequest, processingError.Error(), w, r)
+		return
 	}
-
-	for _, entry := range postedAttributes {
-		if entry.Name != "RiverBankRestoration" && entry.Name != "HillSlopeRestoration" && entry.Name != "GullyRestoration" {
-			baseError := errors.New("Name [" + entry.Name + "] not one of [RiverBankRestoration, HillSlopeRestoration, GullyRestoration]")
-			wrappingError := errors.Wrap(baseError, "v1 POST subcatchment handler")
-			m.Logger().Error(wrappingError)
-			m.RespondWithError(http.StatusBadRequest, wrappingError.Error(), w, r)
-			return
-		}
-
-		if entry.Value != InactiveAction && entry.Value != ActiveAction {
-			baseError := errors.New("For named action [" + entry.Name + "], value [" + entry.Value.(string) + "] not one of [Active,Inactive]")
-			wrappingError := errors.Wrap(baseError, "v1 POST subcatchment handler")
-			m.Logger().Error(wrappingError)
-			m.RespondWithError(http.StatusBadRequest, wrappingError.Error(), w, r)
-			return
-		}
-	}
-
-	for actionIndex, action := range m.model.ManagementActions() {
-		if subCatchment != action.PlanningUnit() {
-			continue
-		}
-
-		for _, entry := range postedAttributes {
-			if entry.Name == string(action.Type()) {
-				if entry.Value == InactiveAction {
-					m.model.SetManagementAction(actionIndex, false)
-					m.model.AcceptAll()
-					m.Logger().Info("Model subcatchment [" + subCatchmentAsString + "], Action [" + entry.Name + "], set to [" + entry.Value.(string) + "]")
-				}
-				if entry.Value == ActiveAction {
-					m.model.SetManagementAction(actionIndex, true)
-					m.model.AcceptAll()
-					m.Logger().Info("Model subcatchment [" + subCatchmentAsString + "], Action [" + entry.Name + "], set to [" + entry.Value.(string) + "]")
-				}
-			}
-		}
-	}
-
-	m.modelSolution = new(solution.SolutionBuilder).
-		WithId(m.model.Id()).
-		ForModel(m.model).
-		Build()
 
 	restResponse := new(rest.Response).
 		Initialise().
@@ -186,4 +120,97 @@ func (m *Mux) v1PostSubcatchmentHandler(w http.ResponseWriter, r *http.Request) 
 		wrappingError := errors.Wrap(writeError, "v1 POST subcatchment handler")
 		m.Logger().Error(wrappingError)
 	}
+}
+
+func (m *Mux) processSubcatchmentPost(w http.ResponseWriter, r *http.Request, subCatchment planningunit.Id) error {
+	scenarioName := m.Attribute(scenarioNameKey).(string)
+	responseMessage := fmt.Sprintf("Processing POST of model [%s] subcatchment [%d] state", scenarioName, subCatchment)
+	m.Logger().Info(responseMessage)
+
+	requestContent := requestBodyToBytes(r)
+	postedAttributes := attributes.Attributes{}
+
+	unmnarshalError := json.Unmarshal(requestContent, &postedAttributes)
+	if unmnarshalError != nil {
+		return errors.Wrap(unmnarshalError, "v1 POST subcatchment handler")
+	}
+
+	verificationError := m.verifyPostedAttributes(postedAttributes)
+	if verificationError != nil {
+		return verificationError
+	}
+
+	m.updateModel(subCatchment, postedAttributes)
+	return nil
+}
+
+func (m *Mux) updateModelSolution() {
+	m.modelSolution = new(solution.SolutionBuilder).
+		WithId(m.model.Id()).
+		ForModel(m.model).
+		Build()
+}
+
+func (m *Mux) verifyPostedAttributes(postedAttributes attributes.Attributes) error {
+	for _, entry := range postedAttributes {
+		if entry.Name != "RiverBankRestoration" && entry.Name != "HillSlopeRestoration" && entry.Name != "GullyRestoration" {
+			baseError := errors.New("Name [" + entry.Name + "] not one of [RiverBankRestoration, HillSlopeRestoration, GullyRestoration]")
+			return errors.Wrap(baseError, "v1 POST subcatchment handler")
+		}
+
+		if entry.Value != InactiveAction && entry.Value != ActiveAction {
+			baseError := errors.New("For named action [" + entry.Name + "], value [" + entry.Value.(string) + "] not one of [Active,Inactive]")
+			return errors.Wrap(baseError, "v1 POST subcatchment handler")
+		}
+	}
+	return nil
+}
+
+func (m *Mux) updateModel(subCatchment planningunit.Id, postedAttributes attributes.Attributes) {
+	for actionIndex, action := range m.model.ManagementActions() {
+		if subCatchment != action.PlanningUnit() {
+			continue
+		}
+
+		for _, entry := range postedAttributes {
+			if entry.Name == string(action.Type()) {
+				if entry.Value == InactiveAction {
+					m.model.SetManagementAction(actionIndex, false)
+				}
+				if entry.Value == ActiveAction {
+					m.model.SetManagementAction(actionIndex, true)
+				}
+				m.model.AcceptAll()
+				infoMessage := fmt.Sprintf("Model subcatchment [%d], Action [%s] set to [%s]", subCatchment, entry.Name, entry.Value)
+				m.Logger().Info(infoMessage)
+			}
+		}
+	}
+	m.updateModelSolution()
+}
+
+func deriveSubCatchmentFrom(r *http.Request) string {
+	pathElements := strings.Split(r.URL.Path, rest.UrlPathSeparator)
+	lastElementIndex := len(pathElements) - 1
+	subCatchmentAsString := pathElements[lastElementIndex]
+	return subCatchmentAsString
+}
+
+func toPlanningUnitId(subCatchmentAsString string) planningunit.Id {
+	subCatchmentAsInteger, convertError := strconv.Atoi(subCatchmentAsString)
+	if convertError != nil {
+		panic("Should not reach here -- regular expression map should stop non-integers from being passed to handler")
+	}
+
+	subCatchment := planningunit.Id(subCatchmentAsInteger)
+	return subCatchment
+}
+
+func (m *Mux) modelContains(subCatchment planningunit.Id) bool {
+	for _, value := range m.modelSolution.PlanningUnits {
+		if value == subCatchment {
+			return true
+		}
+	}
+	return false
 }
