18a54e1ea5dc38fcf42f623a2ade1ae917dc4837
Linds <lindsay.w.bradford@gmail.com>
2021-07-12 13:46:34 +1000

    Ensure internal EncodedActions attribute of Solutions are not encoded to json.

--- internal/pkg/annealing/solution/Solution.go
+++ internal/pkg/annealing/solution/Solution.go
@@ -54,7 +54,7 @@ type Solution struct {
 	ActiveManagementActions   map[planningunit.Id]ManagementActions
 	InactiveManagementActions map[planningunit.Id]ManagementActions `json:"-"`
 
-	EncodedActions string
+	EncodedActions string `json:"-"`
 	attributes.ContainedAttributes
 }
 
